c48f3daa54e9079825a31e9f28033750ac7ee569
Rebecca Robles <rrobles9429@jh-mba158-17.local>
2019-02-12 11:27:12 -0600

    added Item class

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var newItemTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func addNewItemButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = "Hello"
+        return cell
+    }
 
 }
 
