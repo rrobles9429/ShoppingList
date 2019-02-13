cda926ce55da5a99cb88e5e6370f5a36e101165a
Rebecca Robles <rrobles9429@jh-mba158-17.local>
2019-02-13 11:25:55 -0600

    added adding items

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -12,22 +12,37 @@ class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var newItemTextField: UITextField!
+    var items: [Item] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        
+        let item1 = Item(name: "milk")
+        let item2 = Item(name: "eggs")
+        items = [item1, item2]
+        let item3 = Item(name: "bread")
+        items.append(item3)
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func addNewItemButtonPressed(_ sender: UIBarButtonItem) {
+        if let newItemName = newItemTextField.text, newItemName != ""{
+            let newItem = Item(name: newItemName)
+            items.append(newItem)
+            tableView.reloadData()
+        }
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
-        cell.textLabel?.text = "Hello"
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "myCell"){
+        let currentItemName = items[indexPath.row].name
+        cell.textLabel?.text = currentItemName
         return cell
+    }else{
+    return UITableViewCell()
     }
-
+ }
 }
 
